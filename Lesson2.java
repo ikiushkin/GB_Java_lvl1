ec9838b8b7f889d3f2e8d6de647bcbdf7c5e5d59
Igor Kiushkin <54315278+ikiushkin@users.noreply.github.com>
2020-07-02 20:16:10 +0300

    Add files via upload

---
+++ Lesson2.java
@@ -0,0 +1,144 @@
+package ru.geekbrains;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Arrays;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+    }
+
+    // Задание 1
+
+    public static void replacingValue () {
+
+        int[] arrTask1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        for (int i = 0; i < arrTask1.length; i ++) {
+            if (arrTask1[i] == 0) {
+                arrTask1[i] = 1;
+            } else {
+                arrTask1[i] = 0;
+            }
+        }
+    }
+
+    // Задание 2
+
+    public static void addNumbInArray () {
+
+        int[] arrTask2 = new int[8];
+        int arrValue = 0;
+
+        for (int i = 0; i < arrTask2.length; i ++) {
+            arrTask2[i] = arrValue;
+            arrValue += 3;
+        }
+    }
+
+    // Задание 3
+
+    public static void multiplyByTwo () {
+
+        int[] arrTask3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < arrTask3.length; i ++) {
+            if (arrTask3[i] < 6) {
+                arrTask3[i] *= 2;
+            }
+        }
+    }
+
+    // Задание 4
+
+    public static void fillTheArrayWithOne () {
+
+        int[][] arrTask4 = new int[5][5];
+
+        for (int i = 0; i < arrTask4.length; i++) {
+            for (int j = 0; j < arrTask4[i].length; j++) {
+                arrTask4[i][j] = 1;
+            }
+        }
+    }
+
+    // Задание 5
+
+    public static void findMinMax () {
+
+        int[] arrTask5 = {4, 5, 0, 3, 2, 11, -12, 4, 5, 2, 4, 8, 9, 15, 1, -16};
+        int minNumb = arrTask5[0];
+        int maxNumb = 0;
+
+        for (int i = 0; i < arrTask5.length; i++) {
+            if (i == arrTask5.length-1) {
+                break;
+            }
+
+            if (arrTask5[i] < arrTask5[i+1] && maxNumb < arrTask5[i+1]) {
+                maxNumb = arrTask5[i+1];
+            }
+
+            if (arrTask5[i] > arrTask5[i+1] && minNumb > arrTask5[i+1]) {
+                minNumb = arrTask5[i+1];
+            }
+        }
+    }
+
+    // Задание 6
+
+    public static void checkBalance (int[] arrTask6) {
+
+        int leftHalf = arrTask6[0];
+        int rightHalf = arrTask6[arrTask6.length-2] + arrTask6[arrTask6.length-1];
+
+        for (int i = 1; i < arrTask6.length-2; i ++) {
+            leftHalf = leftHalf + arrTask6[i];
+        }
+
+        if (leftHalf == rightHalf) {
+            System.out.println("true");
+        }
+    }
+
+    // Задание 7
+
+    public static int[] createArrayWithShift (int[] arrTask7, int shiftValue) {
+
+        int currentIndex, newIndex, buffer;
+
+        for (int i = 0; i < shiftValue; i ++) {
+            buffer = arrTask7[i];
+
+            currentIndex = i;
+
+            if (shiftValue > 0) {
+                while (true) {
+                    newIndex = currentIndex + shiftValue;
+                    if (newIndex >= arrTask7.length) {
+                        newIndex = newIndex - arrTask7.length;
+                    }
+                    if (newIndex == i) {
+                        break;
+                    }
+                    arrTask7[currentIndex] = arrTask7[newIndex];
+                    currentIndex = newIndex;
+                }
+            } else if (shiftValue < 0) {
+                while (true) {
+                    newIndex = currentIndex + shiftValue;
+                    if (newIndex < 0) {
+                        newIndex =  arrTask7.length - newIndex;
+                    }
+                    if (newIndex == i) {
+                        break;
+                    }
+                    arrTask7[currentIndex] = arrTask7[newIndex];
+                    currentIndex = newIndex;
+                }
+            }
+            arrTask7[currentIndex] = buffer;
+        }
+        return arrTask7;
+    }
+}
