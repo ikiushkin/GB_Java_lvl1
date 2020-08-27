300574b556eb6d4f5ed555682960055ff80c57ef
Igor Kiushkin <brotherall@rambler.ru>
2020-08-27 15:07:15 +0300

    pull1

---
+++ server/service/AuthServiceImpl.java
@@ -0,0 +1,58 @@
+package server.service;
+
+import server.inter.AuthService;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AuthServiceImpl implements AuthService {
+
+    public List<UserEntity> usersList;
+
+    public AuthServiceImpl() {
+        this.usersList = new LinkedList<>();
+        this.usersList.add(new UserEntity("login1", "pass1", "nick1"));
+        this.usersList.add(new UserEntity("login2", "pass2", "nick2"));
+        this.usersList.add(new UserEntity("login3", "pass3", "nick3"));
+        this.usersList.add(new UserEntity("login3", "pass4", "nick4"));
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Сервис аутентификации запущен");
+    }
+
+    @Override
+    public String getNick(String login, String password) {
+        for (UserEntity u : usersList) {
+
+            if (u.login.equals(login) && u.password.equals(password)) {
+                return u.nick;
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Сервис аутентификации остановлен");
+
+    }
+
+    public class UserEntity {
+        private String login;
+        private String password;
+        private String nick;
+
+        public UserEntity(String login, String password, String nick) {
+            this.login = login;
+            this.password = password;
+            this.nick = nick;
+        }
+
+        public String getNick() {
+            return nick;
+        }
+    }
+}
